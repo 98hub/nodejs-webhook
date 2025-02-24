fd8631e27fff0e2cce3a0ba1fdc0a009cebaac65
98hub <akun.sumberweb@gmail.com>
2025-02-24 20:31:27 +0700

    update msg

--- test/app.test.js
+++ test/app.test.js
@@ -2,10 +2,10 @@ const request = require('supertest');
 const { app, server } = require('../app');
 
 describe('GET /', () => {
-    it('should return Hello World!', async () => {
+    it('should return This app is running properly!', async () => {
         const res = await request(app).get('/');
         expect(res.statusCode).toEqual(200);
-        expect(res.text).toContain('Hello World!');
+        expect(res.text).toContain('This app is running properly!');
     });
 });
 
